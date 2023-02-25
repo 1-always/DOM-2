3134ab042f9227c62e6244a15c3b40c9e5864267
1-always <prathyushan.2001@gmail.com>
2023-02-25 13:25:56 +0530

    second commit

--- dom.js
+++ dom.js
@@ -22,6 +22,30 @@ a=['FIRST','SECOND','THIRD','FOURTH'];
 for( let i=0; i<c.length; i++){
     c[i].textContent=a[i];
     c[i].style.fontWeight="bold";
-c[i].style.backgroundColor="lightblue";
+    if(i==2)
+    {
+        c[i].style.backgroundColor="green";
+    }
+    else{
+        c[i].style.backgroundColor="lightblue";
+    }
+
 
 }
+//get element by tag name
+/*let c1=document.getElementsByTagName('li');
+console.log(c1)
+c1[1].textContent="SECOND";
+c1[1].style.fontWeight="bold";
+c1[1].style.backgroundColor="lightblue";
+a1=['FIRST','SECOND','THIRD','FOURTH','FIFTH'];
+for( let i=0; i<c1.length; i++){
+    c1[i].textContent=a1[i];
+    c1[i].style.fontWeight="bold";
+    if(i==2)
+    {
+        c1[i].style.backgroundColor="green";
+    }
+c1[i].style.backgroundColor="lightblue";
+
+}*/
